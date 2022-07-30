2c04a942cbc6d86224cc77f7d798ea672c1bcba3
Lolykece <109230621+Lolykece@users.noreply.github.com>
2022-07-30 09:31:24 +0700

    Add files via upload

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,7 +1,7 @@
 let handler = async m => m.reply(`
 ╭─「 Donasi • Pulsa 」
-│ •  [0895-3303-79186]
-│ •  [0896-5436-0447]
+│ •  [62885731657889]
+│ •  [62885731657889]
 ╰────
 `.trim()) // Tambah sendiri kalo mau
 handler.help = ['donasi']
